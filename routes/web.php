a9ea6674ea644f013c50d46a3cd72c3fb7fd933f
Aman <amankumarop103@gmail.com>
2025-08-09 22:42:19 +0530

    routes added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,51 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('blog/{slug}', function($slug) {
+    dd($slug);
+});
+
+Route::get('blog/{slug}/comment/{commentId}', function($slug, $commentId) {
+    dump($slug);
+    dd($commentId);
+});
+
+Route::get('order/{slug}/comment/{commentId?}', function($slug, $commentId = "123") {
+    dump($slug);
+    dd(@$commentId);
+});
+
+Route::get('order/{id}', function($id) {
+    dump($id);
+    dump(route("orders"));
+})->where("id", "[0-9]+");
+
+Route::get('orders', function($id) {
+    dump($id);
+})->name("orders");
+
+
+Route::prefix("admin")->group(function() {
+    Route::get('/users', function () {
+        return 'Admin Users Page';
+    });
+
+    Route::get('/settings', function () {
+        return 'Admin Settings Page';
+    });
+});
+
+Route::middleware(['web'])->group(function () {
+    Route::get('/profile', function () {
+        return 'Your Profile';
+    });
+
+    Route::get('/orders', function () {
+        return 'Your Orders';
+    });
+});
+
+Route::fallback(function () {
+    return 'Page not found!';
+});
