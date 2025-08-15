36fed36d26612973dfe778e5728c383e833b3dc5
Aman <amankumarop103@gmail.com>
2025-08-15 18:56:09 +0530

    orm done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, "index"]);
+
+Route::get('/users', [UserController::class, "index"]);
